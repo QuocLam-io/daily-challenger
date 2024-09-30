d53a40222a96a61d487bc8dd4b9ce5b1befe0b36
frostycakes <lamquoc.24@gmail.com>
2024-09-29 22:28:23 -0400

    Modal component design system

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,31 +1,46 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./Modal.scss";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Modal = ({ open, close, children }) => {
-  if (!open) return null;
-
   useEffect(() => {
     const handleEscape = (e) => {
-      if (e.key === "Escape") onClose();
+      if (e.key === "Escape") close();
     };
     window.addEventListener("keydown", handleEscape);
     return () => window.removeEventListener("keydown", handleEscape);
   }, [close]);
 
+  //Stops eventlisteners of parents (remember the contacts close button?)
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
   return ReactDOM.createPortal(
-    <>
-      <div className="modal-overlay" onClick={close}></div>
-      <div className="modal-container">
-        <div className="modal-content">
-          <button className="modal-close" onClick={close}>
-            &times;
-          </button>
-          {children}
-        </div>
-      </div>
-    </>,
-    document.body // or a custom portal root
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          className="modal"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          onClick={close}
+        >
+          <div className="modal-overlay" />
+          <div className="modal-container" onClick={stopPropagation}>
+            <div className="modal-content">
+              <button className="modal-close" onClick={close}>
+                &times;
+              </button>
+              {children}
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
+    document.body
   );
 };
 
