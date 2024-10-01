12c85d169673f0f4efca5c9eb3e290c34f901a1c
frostycakes <lamquoc.24@gmail.com>
2024-10-01 16:20:45 -0400

    eslint undef error

--- src/components/modals/ContactsModal.tsx
+++ src/components/modals/ContactsModal.tsx
@@ -3,11 +3,11 @@ import "./ContactsModal.scss";
 import Modal from "@/components/Modal.tsx";
 
 export function ContactsModal({ contactsModalOpen, setContactsModalOpen }) {
-
   return (
-    {/* TODO: Return to dialog */}
+    // Return to dialog
     <Modal
       open={contactsModalOpen}
+      // open={isOpen}
       close={() => {
         setContactsModalOpen(false);
       }}
