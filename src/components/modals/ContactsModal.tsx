0d5799d92b61a70b716f0542ea85c17832e20ee1
frostycakes <lamquoc.24@gmail.com>
2024-10-02 16:51:19 -0400

    can't find tsx *rolls eyes*

--- src/components/modals/ContactsModal.tsx
+++ src/components/modals/ContactsModal.tsx
@@ -2,12 +2,14 @@ import { useRouter } from "next/navigation";
 import "./ContactsModal.scss";
 import Modal from "@/components/Modal.tsx";
 
+
+
 export function ContactsModal({ contactsModalOpen, setContactsModalOpen }) {
   return (
     // Return to dialog
     <Modal
       open={contactsModalOpen}
-      // open={isOpen}
+      // open={undefinedVariable}
       close={() => {
         setContactsModalOpen(false);
       }}
